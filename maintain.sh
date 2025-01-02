aae46a6ca0d5568f643dd5f403711d3eaf60b4a1
aniyamarie <169121284+aniyamarie@users.noreply.github.com>
2025-01-01 21:48:23 -0600

    Update maintain.sh

--- maintain.sh
+++ maintain.sh
@@ -7,6 +7,7 @@ export LC_ALL
 # Converts uppercase to lowercase, sorts, removes duplicates and removes allowlisted domains
 cat disposable_email_blocklist.conf | tr '[:upper:]' '[:lower:]' | sort -f | uniq -i  > temp.conf
 comm -23 temp.conf allowlist.conf > disposable_email_blocklist.conf
+aaniyamarieehgff@oletters.com
 
 rm temp.conf
 echo "Done!"
